1e4591c987f24785ba32665e6915dfb5287d5172
Elliot Saba <staticfloat@gmail.com>
2021-06-22 00:20:43 +0000

    Further iterate on the s3 sync job

--- bin/update_pkgservers.jl
+++ bin/update_pkgservers.jl
@@ -3,6 +3,14 @@ using PkgServerLogAnalysis: get_server_list, get_ssh_creds
 
 for server in PkgServerLogAnalysis.get_server_list()
     @info(server)
-    p = run(`ssh -t -o StrictHostKeyChecking=no $(get_ssh_creds(server)) "source ~/.bash_profile; cd ~/src/PkgServer.jl/deployment; git pull; make"`)
+    cmd = """
+    source ~/.bash_profile
+    cd ~/src/PkgServer.jl/deployment
+    git pull
+    sudo chown --reference=../Project.toml -R .
+    cd deployment
+    make
+    """
+    p = run(`ssh -t -o StrictHostKeyChecking=no $(get_ssh_creds(server)) $(cmd)`)
     @info("update-$(server): $(success(p) ? "✓" : "✘")")
 end
